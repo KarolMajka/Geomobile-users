a8606d30de523a01395a7b9deddcdcdbf2d45a59
Karol Majka <karolmajka95@gmail.com>
2023-11-22 14:09:14 +0100

    Initial Commit

---
+++ Geomobile-users/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geomobile-users
+//
+//  Created by Karol Majka on 22/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
